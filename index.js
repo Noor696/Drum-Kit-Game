94aa99be2d6ced5afea528a718deff05d028ec46
Noor696 <eng.nooralhateeb@gmail.com>
2023-05-15 21:14:29 +0000

    add basic

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var numOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0 ; i<numOfDrumButtons ; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+        alert("clicked");
+    })
+}
